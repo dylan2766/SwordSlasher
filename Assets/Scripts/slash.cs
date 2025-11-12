5b8e83e2706817b994dad0de2cd9d806b3f93658
dylan2766 <reevesdy@sheridancollege.ca>
2025-11-12 11:15:46 -0500

    - Worked on the prototype

--- Assets/Scripts/slash.cs
+++ Assets/Scripts/slash.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class slash : MonoBehaviour
 {
+    public Vector2 mousePos;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,8 @@ public class slash : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
+        Debug.Log(mousePos);
     }
 }
