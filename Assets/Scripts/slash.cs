8f92f390640e1d44b5f30034cd22df839e80b278
dylan2766 <reevesdy@sheridancollege.ca>
2025-11-14 15:50:47 -0500

    Saved

--- Assets/Scripts/slash.cs
+++ Assets/Scripts/slash.cs
@@ -5,6 +5,10 @@ public class slash : MonoBehaviour
 {
     public Vector2 mousePos;
 
+    //public bool collisionDetected = false;
+
+    public int score = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,9 +21,31 @@ public class slash : MonoBehaviour
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = mousePos;
 
+        //if (collisionDetected == true)
+        //{
+        //    score = score + 20;
+        //}
+
         if (Input.GetMouseButtonUp(0))
         {
             Destroy(gameObject);
         }
     }
+    //public void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    Debug.Log("This object has just collided with another.");
+    //    collisionDetected = true;
+    //}
+
+    //public void OnCollisionStay2D(Collision2D collision)
+    //{
+    //    Debug.Log("This object is currently touching another.");
+    //    collisionDetected = true;
+    //}
+
+    //public void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    Debug.Log("This object has stopped colliding with another.");
+    //    collisionDetected = false;
+    //}
 }
---
+++ Assets/Scripts/targets.cs
@@ -0,0 +1,28 @@
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class targets : MonoBehaviour
+{
+    public CircleCollider2D tR;
+    public CircleCollider2D tL;
+    public CircleCollider2D bR;
+    public CircleCollider2D bL;
+
+    public SpriteRenderer topR;
+    public SpriteRenderer topL;
+    public SpriteRenderer botR;
+    public SpriteRenderer botL;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if( == true)
+    }
+}
